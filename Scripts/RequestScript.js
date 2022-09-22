709c4311bba38bce39dfc1d229f764a9c667b788
George Lin <86717650+georgel2020@users.noreply.github.com>
2022-09-22 20:30:56 +0800

    Create RequestScript.js

---
+++ Scripts/RequestScript.js
@@ -0,0 +1,4 @@
+// Request Script: Send request to chromedino.com to upload score
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '/inc/set.php?name=' + 'Username' + '&score=' + 199999, false); // Change 'Username' to your name and change '199999' to your score (a value more than 199999 will be considered to hack the game)
+xhr.send();
