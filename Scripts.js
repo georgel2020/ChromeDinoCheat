5dfc03b1abe5da03efcd69a471b2007f93c651bd
George Lin <86717650+georgel2020@users.noreply.github.com>
2022-09-22 19:52:08 +0800

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,49 +1,60 @@
-//Let dino stay immortal
-Runner.instance_.gameOver = function() {}
-
-//Change dino's speed
-Runner.instance_.setSpeed(1048576)
-
-//Auto run
+// Auto script: Automatically control the dino to jump or duck
 function TrexRunnerBot() {
-const makeKeyArgs = (keyCode) => {
-const preventDefault = () => void 0; return {keyCode, preventDefault}; };
-const upKeyArgs = makeKeyArgs(38); const downKeyArgs = makeKeyArgs(40); const startArgs = makeKeyArgs(32);
-if (!Runner().playing) {
-Runner().onKeyDown(startArgs); setTimeout(() => {
-Runner().onKeyUp(startArgs); }, 500); }
-function conquerTheGame() {
-if (!Runner || !Runner().horizon.obstacles[0]) return;
-const obstacle = Runner().horizon.obstacles[0];
-if (obstacle.typeConfig && obstacle.typeConfig.type === 'SNACK') return;
-if (needsToTackle(obstacle) && closeEnoughToTackle(obstacle)) tackle(obstacle); }
-function needsToTackle(obstacle) {
-return obstacle.yPos !== 50; }
-function closeEnoughToTackle(obstacle) {
-return obstacle.xPos <= Runner().currentSpeed * 18; }
-function tackle(obstacle) {
-if (isDuckable(obstacle)) {
-duck(); } else {
-jumpOver(obstacle); }
+    const makeKeyArgs = (keyCode) => {
+        const preventDefault = () => void 0; return { keyCode, preventDefault };
+    };
+    const upKeyArgs = makeKeyArgs(38); const downKeyArgs = makeKeyArgs(40); const startArgs = makeKeyArgs(32);
+    if (!Runner().playing) {
+        Runner().onKeyDown(startArgs); setTimeout(() => {
+            Runner().onKeyUp(startArgs);
+        }, 500);
+    }
+    function conquerTheGame() {
+        if (!Runner || !Runner().horizon.obstacles[0]) return;
+        const obstacle = Runner().horizon.obstacles[0];
+        if (obstacle.typeConfig && obstacle.typeConfig.type === 'SNACK') return;
+        if (needsToTackle(obstacle) && closeEnoughToTackle(obstacle)) tackle(obstacle);
+    }
+    function needsToTackle(obstacle) {
+        return obstacle.yPos !== 50;
+    }
+    function closeEnoughToTackle(obstacle) {
+        return obstacle.xPos <= Runner().currentSpeed * 18;
+    }
+    function tackle(obstacle) {
+        if (isDuckable(obstacle)) {
+            duck();
+        } else {
+            jumpOver(obstacle);
+        }
+    }
+    function isDuckable(obstacle) {
+        return obstacle.yPos == 75;
+    }
+    function duck() {
+        drop(); Runner().onKeyDown(downKeyArgs);
+        setTimeout(() => {
+            Runner().onKeyUp(downKeyArgs);
+        }, 500);
+    }
+    function drop() {
+        Runner().onKeyDown(downKeyArgs);
+        Runner().onKeyUp(downKeyArgs);
+    }
+    function jumpOver(obstacle) {
+        if (isNextObstacleCloseTo(obstacle))
+            jumpFast(); else
+            Runner().onKeyDown(upKeyArgs);
+    }
+    function isNextObstacleCloseTo(currentObstacle) {
+        const nextObstacle = Runner().horizon.obstacles[1];
+        return nextObstacle && nextObstacle.xPos - currentObstacle.xPos <=
+            Runner().currentSpeed * 42;
+    }
+    function jumpFast() {
+        Runner().onKeyDown(upKeyArgs); Runner().onKeyUp(upKeyArgs);
+    }
+    return { conquerTheGame: conquerTheGame };
 }
-function isDuckable(obstacle) {
-return obstacle.yPos == 75; }
-function duck() {
-drop(); Runner().onKeyDown(downKeyArgs);
-setTimeout(() => {
-Runner().onKeyUp(downKeyArgs); }, 500); }
-function drop() {
-Runner().onKeyDown(downKeyArgs);
-Runner().onKeyUp(downKeyArgs); }
-function jumpOver(obstacle) {
-if (isNextObstacleCloseTo(obstacle))
-jumpFast(); else
-Runner().onKeyDown(upKeyArgs); }
-function isNextObstacleCloseTo(currentObstacle) {
-const nextObstacle = Runner().horizon.obstacles[1];
-return nextObstacle && nextObstacle.xPos - currentObstacle.xPos <=
-Runner().currentSpeed * 42; }
-function jumpFast() {
-Runner().onKeyDown(upKeyArgs); Runner().onKeyUp(upKeyArgs); }
-return {conquerTheGame: conquerTheGame}; }
-let bot = TrexRunnerBot(); let botInterval = setInterval(bot.conquerTheGame, 2);
+let bot = TrexRunnerBot();
+let botInterval = setInterval(bot.conquerTheGame, 2);
