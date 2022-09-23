35b0c43802ed77aad0ade2b31986908cf799fca1
George Lin <86717650+georgel2020@users.noreply.github.com>
2022-09-23 13:13:00 +0800

    Create HighestScoreScore.js

---
+++ Scripts/HighestScoreScore.js
@@ -0,0 +1,3 @@
+// Highest Score Script: Change the highest score
+Runner.instance_.highestScore = Math.ceil(1048576); // Change the value '1048576' to any value you want (this value is not the game score)
+Runner.instance_.distanceMeter.setHighScore(Math.ceil(1048576)); // Change the value '1048576' to any value you want (this value is not the game score)
